1862c8ce5cced8d199f118deae2126f221108290
Gleb LAgutko <gleblagutko@gmail.com>
2021-07-02 15:37:18 +0300

    Initial commit

---
+++ src/main/java/com/starsoft/archivator/ArchivatorApplication.java
@@ -0,0 +1,13 @@
+package com.starsoft.archivator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchivatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/starsoft/archivator/ArchivatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starsoft.archivator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
